f3e9f21c4727680235104a5e74ea3806501bcdbe
Markus Klooth <markusair@markusair.local>
2011-09-15 00:08:12 -0700

    added index

---
+++ index.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<meta http-equiv="Content-type" content="text/html; charset=utf-8">
+		<title>My site</title>
+		
+	</head>
+	<body id="index" onload="">
+		<div>
+			fds
+		</div>
+	</body>
+</html>
